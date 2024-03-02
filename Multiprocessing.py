abdd58827050c5e34887b917e76ae0fc13a834df
Nadasobhy <132937918+Nadasobhy@users.noreply.github.com>
2024-03-02 03:09:52 +0200

    Multiprocessing.py

--- Multiprocessing.py
+++ Multiprocessing.py
@@ -1,19 +1,20 @@
-import threading
 import time
+import multiprocessing
 
 def cal_square(start,end):
     for n in range (1,10000):
-        time.sleep(0.5)
+        time.sleep(0.2)
         print("Square of", n, "is:", n * n)
 
 if __name__ == "__main__":
     
     start = time.perf_counter()
     end = time.perf_counter()
-    thread = threading.Thread(target=cal_square, args=(start, end ))
-    thread.start()
-    thread.join()
+    
+    Process = multiprocessing.Process(target=cal_square, args=(start, end ))
+    Process.start()
+    Process.join()
+    
     total_time = end - start
 
     print("Total time:", total_time)
-
